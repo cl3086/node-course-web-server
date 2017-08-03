0c5b5a767ca3114fb3180fa0feec6386383d1ef0
Calvin Lin <cl3086@nyu.edu>
2017-08-03 02:39:55 -0400

    Modified server.js to show webpage.

--- server.js
+++ server.js
@@ -24,21 +24,21 @@ app.set("view engine", "hbs");
 // order of middleware matters!
 
 // next exists tells express when the middleware is done
-app.use((req, res, next) => {
-    var now = new Date().toString();
-
-    var log = `${now}: ${req.method} ${req.url}`;
-    fs.appendFile("server.log", log + "\n", (err) => {
-        if(err){
-            console.log("Unable to append to server.log.");
-        }
-    });
-    next();
-});
-
-app.use((req, res, next) => {
-    res.render("maintenance.hbs");
-});
+// app.use((req, res, next) => {
+//     var now = new Date().toString();
+//
+//     var log = `${now}: ${req.method} ${req.url}`;
+//     fs.appendFile("server.log", log + "\n", (err) => {
+//         if(err){
+//             console.log("Unable to append to server.log.");
+//         }
+//     });
+//     next();
+// });
+//
+// app.use((req, res, next) => {
+//     res.render("maintenance.hbs");
+// });
 
 app.use(express.static(__dirname + "/public"));
 
